eb2bccc8e71335016bdf817a2551710623fd6274
pessyfeder <pessyf19@gmail.com>
2024-09-23 16:56:27 -0400

    1st commit trial

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -1,7 +1,10 @@
 use humanbodydb
 go
+drop table if exists body
+go
 create table dbo.body(
     id int not null identity primary key, 
+    creaturetype varchar(25) not null,
     bodypart varchar(100) not null, 
     num int not null
     )
