a03bb471bc56114e98f0f530eee776243f35e39d
pessyfeder <pessyf19@gmail.com>
2024-09-23 16:59:22 -0400

    Update 1 table.sql

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -4,6 +4,7 @@ drop table if exists body
 go
 create table dbo.body(
     id int not null identity primary key, 
+    planet not null varchar(50),
     bodypart varchar(100) not null, 
     num int null
     )
