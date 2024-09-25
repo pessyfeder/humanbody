2e45a854139c6fb078b9bca42dae23364d1325c8
pessyfeder <pessyf19@gmail.com>
2024-09-25 14:28:48 -0400

    Update 2 sample data.sql

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -4,7 +4,7 @@ insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 
 union select 'nose',
-union select 'mouth',
+union select 'muoth',
 union select 'teeth', 
 union select 'toungue',
 union select 'brain', 
